9784b50b6aebfd44bd66e447db2e34eee68bea5c
raph <raphael0803@msn.com>
2021-10-12 21:02:10 +0200

    list trigger

---
+++ macros/FindListTriggers.cpp
@@ -0,0 +1,163 @@
+#include <vector>
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <iterator>
+#include <algorithm>
+#include "FindListTriggers.h"
+
+
+using namespace std; 
+
+
+int ListNameTriggers::ListNameTriggersReverse(string name){
+	int cs = 0;
+	
+	for (int i = 0 ; i < triggerName->size() ; i++){
+		if(triggerName->at(i) == name){
+			return cs;
+
+		}
+		cs+=1;
+
+	}
+	return 9999;
+
+}
+
+int ListNameTriggers::IsInList(string name){
+	int ct=0;
+	cout << InfoTriggers.size() << endl;
+	for(int i = 0 ; i < InfoTriggers.size() ; i++){
+		//cout << i << endl;
+		if(InfoTriggers[i].first == name){
+			return ct;
+		}
+		ct+=1;
+	}
+	return 9999;
+}
+
+
+void ListNameTriggers::FindAllNames(){
+	
+	ofstream CompleteList;
+	string DataType = "Stop2400";
+	string NameList = "CompleteList";
+	string ExtTxt = ".txt";
+	string PresList = "PrescaledSubList";
+	string NamePrescaledList = PresList + DataType + ExtTxt;
+	string NameCompleteList = NameList + DataType + ExtTxt;
+
+	CompleteList.open (NameCompleteList.c_str()); //change name
+
+
+	ofstream PrescaledSubList;
+	PrescaledSubList.open (NamePrescaledList.c_str());
+
+	ofstream PrescaledTurnedBad;
+	PrescaledTurnedBad.open ("PrescaledTurnedBad.txt");
+
+	int testcount=0;
+	
+	Long64_t nentries = fChain->GetEntriesFast();
+	Long64_t nbytes = 0, nb = 0, nbi = 0;
+	Long64_t initializing = LoadTree(0); 
+
+	cout << "Number of entries : " << nentries << endl;
+	
+	if (initializing < 0) cout << "Aborted"<< endl;
+
+	nbi = fChain->GetEntry(initializing);   nbytes += nbi;
+	
+	cout << " ntrigger  : " << ntrigger << endl;
+	
+
+
+	for(int i=0; i< ntrigger; i++){
+		cout << "TriggerName " << i << " : " << triggerName->at(i) << endl;
+		
+		//cout << prescaleTrigger[i] << endl;
+		
+		if(prescaleTrigger[i]==1){
+			InfoTriggers.push_back(make_pair(triggerName->at(i), make_pair(true,true)));
+
+		//	MapOfTriggerNames[triggerName->at(i)] = make_pair(true,true);
+		}
+		else{
+			InfoTriggers.push_back(make_pair(triggerName->at(i), make_pair(false,true)));
+
+		//	MapOfTriggerNames[triggerName->at(i)] = make_pair(false,true);
+			PrescaledTurnedBad << "Prescale " << i << " is not 1" << endl;
+		}
+
+
+	}
+	
+	PrescaledTurnedBad << "----------------------------------------------------------------\n ----------------------------------------------------------------\n " << endl;
+	cout << "before loop"<<nentries<<endl;
+	
+	for (Long64_t jentry=0; jentry<nentries;jentry++) { //All entries
+		Long64_t ientry = LoadTree(jentry);
+		if(jentry!=0 && jentry%1000==0) cout << "+1k" << " => " << jentry << " , "<<(jentry*1.0/nentries)*100 << " %" << endl;
+		if (ientry < 0) break;
+        	nb = fChain->GetEntry(jentry);   nbytes += nb;	// 
+		testcount+=1;
+		cout << "in loop" << ntrigger << endl;
+		
+		//cout << "----------------------------- new event -----------------------------" << endl;
+		//if(ntrigger > InfoTriggers.size()){
+			//cout << " --------NEW EVENT ------- "<< endl;
+			for(int i=0; i< ntrigger; i++){
+				cout << i <<"is ok"<< triggerName->at(i) <<  endl;
+				int b = IsInList(triggerName->at(i));
+				//cout <<"after IsInList"<<endl;
+				if(b!=9999){
+					if(prescaleTrigger[i]==1){
+						//cout << triggerName->at(i) << " has prescale 1"  << endl;
+					}
+					else{
+						InfoTriggers[b] = make_pair(triggerName->at(i) , make_pair(false,true));
+						//cout << triggerName->at(i) << " has prescale " << prescaleTrigger[i] << endl;
+					}
+
+				}
+
+				else if(b==9999){
+					//if(prescaleTrigger[i]==1){
+					cout << "Added one vector to the list that wasnt there before, prescale false" << endl;
+					InfoTriggers.push_back(make_pair(triggerName->at(i),make_pair(false,false)));
+				
+				}
+
+			}
+	//cout <<"end loop ntrigger"<<endl;
+	}
+
+	for ( int it = 0 ; it < InfoTriggers.size() ; it++){
+		CompleteList << InfoTriggers[it].first << " " << endl;
+		if(InfoTriggers[it].second.first){
+			PrescaledSubList << InfoTriggers[it].first << endl; //<< " " << itr->second.first << " " << itr->second.second
+		}
+	}
+
+
+	CompleteList.close();
+	PrescaledSubList.close();
+	PrescaledTurnedBad.close();
+}
+
+
+
+
+int main(){
+
+	ListNameTriggers c;
+	c.FindAllNames();
+
+}
+
+
---
+++ macros/FindListTriggers.h
@@ -0,0 +1,177 @@
+#ifndef LISTNAMETRIG_H
+#define LISTNAMETRIG_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <TH2.h>
+#include <TH1.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TPad.h>
+#include <TFile.h>
+#include <map>
+#include <TEfficiency.h>
+
+using namespace std;
+
+class ListNameTriggers
+{
+ public :
+
+
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+   //----------------------------------------
+   //----------------METHODS ----------------
+   //----------------------------------------
+   
+   ListNameTriggers(TTree *tree=0); 
+   ~ListNameTriggers();
+
+ 
+  /* Int_t    GetEntry(Long64_t entry);
+   Long64_t LoadTree(Long64_t entry);
+   void Init(TTree *tree);
+   void Loop();
+   Bool_t Notify();
+   void Show(Long64_t entry = -1);*/
+
+
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+   int ListNameTriggersReverse(string name);
+   void FindAllNames();
+   int IsInList(string name);
+
+   // Declaration of leaf types
+
+   Int_t	ntrigger;
+   Int_t	nhscp;
+
+   Float_t	prescaleTrigger[2000];
+   Bool_t	passTrigger[2000];
+   Int_t	nmuons;
+   Float_t      muon_phi[32];
+   Float_t      muon_eta[32];
+   Float_t      muon_pt[32];
+   vector<string>* triggerName;
+
+   
+    // List of branches
+
+   TBranch        *b_ntrigger; //!
+   TBranch        *b_nhscp;  //!
+   TBranch        *b_passTrigger; //!
+   TBranch        *b_prescaleTrigger;
+   TBranch	  *b_triggerName;
+   TBranch        *b_nmuons;
+   TBranch        *b_muon_eta;   //!
+   TBranch        *b_muon_phi;
+   TBranch        *b_muon_pt;
+
+ private : 
+
+
+   map<string, pair<bool,bool> > MapOfTriggerNames;
+
+   vector< pair<string, pair<bool,bool> > > InfoTriggers;
+   
+};
+
+
+
+ListNameTriggers::ListNameTriggers(TTree *tree) : fChain(0) //constructeur
+{
+	triggerName = 0;
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+	if (tree == 0) {
+		TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("/opt/sbg/cms/safe1/cms/rhaeberl/CMSSW_10_6_2/src/HSCPtriggerStudies/all_stop2400.root");
+		if (!f || !f->IsOpen()) {
+			f = new TFile("/opt/sbg/cms/safe1/cms/rhaeberl/CMSSW_10_6_2/src/HSCPtriggerStudies/all_stop2400.root");
+		}
+		TDirectory * dir = (TDirectory*)f->Get("/opt/sbg/cms/safe1/cms/rhaeberl/CMSSW_10_6_2/src/HSCPtriggerStudies/all_stop2400.root:/stage");
+		dir->GetObject("ttree",tree);
+
+	}
+   
+   Init(tree);
+
+}
+
+
+ListNameTriggers::~ListNameTriggers() //deconstructeur
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+   
+   //delete[] passTrigger;
+}
+
+Int_t ListNameTriggers::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+
+Long64_t ListNameTriggers::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void ListNameTriggers::Init(TTree *tree)
+{
+
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("nmuons", &nmuons, &b_nmuons);
+   fChain->SetBranchAddress("ntrigger", &ntrigger, &b_ntrigger);
+   fChain->SetBranchAddress("prescaleTrigger", prescaleTrigger, &b_prescaleTrigger);
+   fChain->SetBranchAddress("nameTrigger", &triggerName, &b_triggerName);
+   fChain->SetBranchAddress("passTrigger", passTrigger, &b_passTrigger); // & devant PT 1
+   fChain->SetBranchAddress("nhscp", &nhscp, &b_nhscp);
+   fChain->SetBranchAddress("muon_phi", muon_phi, &b_muon_phi);
+   fChain->SetBranchAddress("muon_eta", muon_eta, &b_muon_eta);
+   fChain->SetBranchAddress("muon_pt", muon_pt, &b_muon_pt);
+
+ //  fChain->SetBranchAddress("hscp_muon_idx", hscp_muon_idx, &b_hscp_muon_idx); 
+   Notify();
+}
+
+Bool_t ListNameTriggers::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void ListNameTriggers::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+
+#endif 
