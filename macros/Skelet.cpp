15f2b9975fd88eccf315dcfd5afafd68ccbb5d0c
Raphael <raphael0803@msn.com>
2021-10-14 10:12:48 +0200

    a

--- macros/Skelet.cpp
+++ macros/Skelet.cpp
@@ -539,7 +539,10 @@ void AnaEff::AssoGenId(int indexcandidate,bool trig1){
 
 		cand1.SetPtEtaPhiM(gen_pt[candidatesrh[candidatesrh.size()-1]],gen_eta[candidatesrh[candidatesrh.size()-1]],gen_phi[candidatesrh[candidatesrh.size()-1]],TheorMass);
 cand2.SetPtEtaPhiM(gen_pt[candidatesneutral[candidatesneutral.size()-1]],gen_eta[candidatesneutral[candidatesneutral.size()-1]],gen_phi[candidatesneutral[candidatesneutral.size()-1]],TheorMass);
-		double v = sqrt((cand1+cand2).Dot((cand1+cand2)));
+		
+		
+		homemet = cand1 + cand2;
+		double v = sqrt(homemet.Dot(homemet));
 		double a = cand1.Angle(cand2.Vect());
 		cout << " Angle between both vectors : " << a << " norm of the dot product = " << v << " , and reco pfMET = : " << pfmet_pt[0] << endl;
 		
