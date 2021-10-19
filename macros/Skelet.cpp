b5737c6bc4ff639bd4626e7f00a4512d0d02ce8f
Raphael <raphael0803@msn.com>
2021-10-19 10:27:29 +0200

    t

--- macros/Skelet.cpp
+++ macros/Skelet.cpp
@@ -320,7 +320,7 @@ void AnaEff::Loop()
 					}
 
 				}*/
-				cout << " p / m = " << (track_p[hscp_track_idx[indexcandidatesel]]*1.0) << " / " << TheorMass << = (track_p[hscp_track_idx[indexcandidatesel]]*1.0/TheorMass) <<endl;
+				cout << " p / m = " << (track_p[hscp_track_idx[indexcandidatesel]]*1.0) << " / " << TheorMass <<" = " << (track_p[hscp_track_idx[indexcandidatesel]]*1.0/TheorMass) <<endl;
 
 				if( (track_p[hscp_track_idx[indexcandidatesel]]*1.0/TheorMass) < 0.1){
 					nbinfpom+=1;
@@ -332,7 +332,7 @@ void AnaEff::Loop()
 					cout << " nb > 0.9 " << nbsuppom << endl;
 				}
 
-				if( (track_p[hscp_track_idx[indexcandidatesel]]*1.0/TheorMass) < 0.9 && ((track_p[hscp_track_idx[indexcandidatesel]]*1.0/TheorMass) >0.1)){
+				if( (track_p[hscp_track_idx[indexcandidatesel]]*1.0/TheorMass) < 0.9 && (track_p[hscp_track_idx[indexcandidatesel]]*1.0/TheorMass) >0.1){
 					nbinpom+=1;
 					cout << " 0.9 < nb < 0.1 " << nbinpom << endl;
 				}
