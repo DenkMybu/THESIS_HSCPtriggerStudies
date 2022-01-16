e5c0f454ed0ab23f8f0a364512f53f36942e800d
Raph mac <raphael0803@msn.com>
2022-01-16 21:03:51 +0100

    a

--- src/Inc.cpp
+++ src/Inc.cpp
@@ -278,7 +278,7 @@ void TrigEff::WritePlots(string NameVar,string NameOfFile){ //TFile* OutputHisto
 	OutputHisto->cd();
 	for(int j=0; j < EffvsObsAll.size() ; j++){
 		EffvsObsAll[j]->Write();
-		EffvsRecoCalo->Write();
+		EffvsRecoCalo[j]->Write();
 		EffvsPom[j]->Write();
 		EffvsPt[j]->Write();
 	}
